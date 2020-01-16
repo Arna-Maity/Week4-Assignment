62bca65ae847fe4bbf8693518c06c43259e73ed2
Arna-Maity <arnamaity6230@gmail.com>
2020-01-16 20:31:17 +0530

    Organized the directory structure and reconfigured the build system [WIP].

--- course1/src/sources.mk
+++ course1/src/sources.mk
@@ -11,8 +11,11 @@
 
 # Add your Source files to this variable
 SOURCES = main.c \
-	  memory.c 
-
+	  	  memory.c \
+		  data.c \
+		  stats.c \
+		  course1.c
+		  
 # Add your include paths to this variable
 INCLUDES = -I../include/common/
 
